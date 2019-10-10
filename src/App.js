9f68ca92b6a130b4a402a35dee1740fe0bdd4d7d
Superdref <frederiquemendy2205@gmail.com>
2019-10-10 16:46:10 +0200

    chat-app is ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Contact from './components/Contact';
 
-function App() {
+function App({props}) {
   return (
     <div className="App">
-      
+        <Contact className="Contact"
+          avatar="https://randomuser.me/api/portraits/women/10.jpg"
+          name="Kim Mendoza"
+          isOnline
+        />
+        <Contact className="Contact"
+          avatar="https://randomuser.me/api/portraits/women/49.jpg"
+          name="Tamara Neal"
+        />
+        <Contact className="Contact"
+          avatar="https://randomuser.me/api/portraits/men/63.jpg"
+          name="Gavin Weaver"
+          isOnline
+        />   
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import './Contact.css';
 
-function Contact() {
+const Contact = ({avatar, name, isOnline}) => {
+    const onlineClass = isOnline ? "status-online" : "status-offline"
+    const textOnline = isOnline ? "online" : "offline"
+    
     return (
+        <div className='Contact'>
+            <img src={avatar} alt='User' className="avatar" />
+            <div>
+                <h1 className='name'>{name}</h1>
+                <div className="status">
+                    <div className={onlineClass}></div>
+                    <div className="status-text">{textOnline}</div>
+                </div>
 
+            </div>
+        </div>
     );
 }
 export default Contact;
